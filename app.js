2e10fd79f109e1c9828472151738ce1338c7b8a8
Sarah Rockhill <sarahrockhill@Sarahs-MacBook-Pro.local>
2020-10-29 01:05:27 -0400

    stuff

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+
 var corsApiUrl = "https://cors-anywhere.herokuapp.com/";
-// TODO: REPLACE YOUR TOKEN
-var apiToken = "?token=YOUR_TOKEN_HERE";
+
+var apiToken = "?token=1FClclFeo95S938GKdPNXw5Cw1TtcDWDQ-RxbUDFRIk";
+
 
 // CORS stands for "cross origin resource sharing" -- you'll be making http requests in order
 // DON'T CHANGE THIS: fetches the data from the API endpoint
@@ -17,15 +19,111 @@ const corsPromise = () =>
     const request = doCORSRequest({
       url: "https://trefle.io/api/v1/plants" + apiToken,
     });
+    
     resolve(request);
   });
 
-// THIS IS SOME SAMPLE CODE FOR HOW TO USE PROMISES -- feel free to adapt this into a function!
+
 corsPromise().then(
   (request) =>
     (request.onload = request.onerror = function () {
-      // TODO: ADD FUNCTION, ETC. FOR WHATEVER YOU WANT TO DO ONCE THE DATA IS RECEIVED
+      const jsonified = JSON.parse(request.response);
+      const plants = jsonified.data
+      
+      handleResponse(plants)
+     
     })
 );
 
-//// TODO: ADD WHATEVER FUN CONTENT YOU WANT ////
+let familyDict = []
+let all_plants;
+
+const handleResponse = (plants) => {
+
+  console.log(plants)
+  all_plants = plants;
+  
+  let btn = document.createElement("button")
+  btn.classList.add("btn", "btn-success")
+  btn.innerHTML = "All plants"
+  btn.addEventListener("click", function (){
+    plants.map(turnOn)
+  })
+  document.getElementById("btn-div").appendChild(btn)
+  plants.map(makePlantDiv)
+  plants.map(addFamily)
+};
+
+const makePlantDiv = (data) => {
+  let div = document.createElement("div");
+    let col = makeRowandCol()
+    col.classList.add("text-center")
+    div.classList.add("card", "plant-div", "mx-auto", "my-4","text-center")
+    col.appendChild(div)
+    addData(data, div)
+
+
+}
+
+const makeRowandCol = () => {
+  let row = document.createElement("div");
+  row.classList.add("row");
+  let col = document.createElement("div")
+  col.classList.add("col")
+  row.appendChild(col)
+  document.getElementById("container").appendChild(row)
+  return col
+}
+
+const addData = (data, div) => {
+  let title = document.createElement("p")
+  title.classList.add("title")
+  div.appendChild(title)
+  title.innerHTML = data.common_name
+  div.id = data.id
+  let img = document.createElement("img")
+  img.classList.add("img-fluid")
+  
+  img.src = data.image_url
+  div.appendChild(img)
+  
+
+
+
+}
+
+const addFamily = (plant) => {
+  if (!familyDict.includes(plant.family)){
+      familyDict.push(plant.family)
+      let btn = document.createElement("button")
+      btn.classList.add("btn", "btn-primary")
+      btn.innerHTML = plant.family
+      document.getElementById("btn-div").append(btn)
+      btn.addEventListener("click", function () {
+        let family = all_plants.filter((plnt) => {
+          return plnt.family === plant.family
+        })
+        family.map(turnOn)
+        let nonfamily = all_plants.filter((plnt) => {
+          return plnt.family !== plant.family
+        })
+        nonfamily.map(turnOff)
+
+
+
+      })
+  }
+
+}
+
+const turnOn = (plant) =>{
+  document.getElementById(plant.id).classList.remove("d-none")
+
+}
+
+const turnOff = (plant) =>{
+  document.getElementById(plant.id).classList.add("d-none")
+
+}
+
+
